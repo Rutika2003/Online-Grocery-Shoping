df1a1f6769ff284645db8aaea2678bbaa844637e
Rutika Rajannavar <123802364+Rutika2003@users.noreply.github.com>
2025-07-22 17:27:25 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,9 @@
+            <div class="hk-footer-wrap container">
+                <footer class="footer">
+                    <div class="row">
+                        <div class="col-md-6 col-sm-12">
+<p>Project by : <a href="https://phpgurukul.com/" class="text-dark text-black">ASHWINI | NANDINI | RUTIKA | SRUSHTI |</a> ©2024</p>
+                        </div>
+                    </div>
+                </footer>
+            </div>
